75ca56b31c637913c446310cf2d50fb8f851b973
tiffanyluu <tiffanyluu555@gmail.com>
2025-09-05 20:53:32 -0500

    Add mobile responsive fixes and hamburger menu

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,29 @@
-// webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production", // Change to production
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  // devtool: "eval-source-map",
   devtool: "source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css", // This will create hashed CSS files
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"], // Replace style-loader
       },
       {
         test: /\.html$/i,
