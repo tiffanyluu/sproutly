d8b365c48aa94d1a2e955f0b338e0a1cb857db5b
tiffanyluu <tiffanyluu555@gmail.com>
2025-09-05 21:31:11 -0500

    Updates

--- index.js
+++
@@ -1,8 +0,0 @@
-import "./styles.css";
-import "./project-dom.js";
-import "./task-dom.js";
-import { displayProjectOnMain, displayProjectOnSidebar, displayTaskOnMain, initialDisplay } from "./display-dom.js";
-import './home-dom.js';
-import { projects, createProject, createTask, deleteProject, deleteTask, getAllTasks, getTodayTasks, getFutureTasks, getStarredTasks } from "./logic.js";
-
-initialDisplay();
